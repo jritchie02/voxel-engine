b75687b1add5f69e46f2311472a24090933bd8b0
Jared Ritchie <jaredritchie@Jareds-Mac-mini.local>
2024-10-18 13:45:00 -0400

    REF: Folder organization to remove part1 nested folder and old README

--- build.py
+++ build.py
@@ -3,10 +3,9 @@ import os
 import platform
 
 # (1)==================== COMMON CONFIGURATION OPTIONS ======================= #
-COMPILER="g++ -g -std=c++17"   # The compiler we want to use 
-                                #(You may try g++ if you have trouble)
-SOURCE="./src/*.cpp"    # Where the source code lives
-EXECUTABLE="project"        # Name of the final executable
+COMPILER="g++ -std=c++17"   # The compiler we want to use 
+SOURCE="./src/*.cpp"        # Where the source code lives
+EXECUTABLE="engine"        # Name of the final executable
 # ======================= COMMON CONFIGURATION OPTIONS ======================= #
 
 # (2)=================== Platform specific configuration ===================== #
@@ -21,12 +20,12 @@ if platform.system()=="Linux":
     LIBRARIES="-lSDL2 -ldl"
 elif platform.system()=="Darwin":
     ARGUMENTS="-D MAC" # -D is a #define sent to the preprocessor.
-    INCLUDE_DIR="-I ./include/ -I/Library/Frameworks/SDL2.framework/Headers  -I./include/glm/"
-    LIBRARIES="-F/Library/Frameworks -framework SDL2"
+    INCLUDE_DIR = "-I ./include/ -I/opt/homebrew/include/SDL2 -I./include/glm/"
+    LIBRARIES = "-L/opt/homebrew/lib -lSDL2"
 elif platform.system()=="Windows":
     ARGUMENTS="-D MINGW -static-libgcc -static-libstdc++" 
     INCLUDE_DIR="-I./include/ -I./include/glm/"
-    EXECUTABLE="project.exe"
+    EXECUTABLE="engine.exe"
     LIBRARIES="-lmingw32 -lSDL2main -lSDL2 -mwindows -mconsole"
 # (2)=================== Platform specific configuration ===================== #
 
@@ -42,21 +41,10 @@ print("========================================================================"
 # Run our command 
 # Here I am using an exit_code so you can
 # also compile & run in one step as
-# python3 build.py && ./lab
-# If compilation failes, ./lab will not run.
+# python3 build.py && ./engine
+# If compilation failes, ./engine will not run.
 exit_code = os.system(compileString)
 exit(0 if exit_code==0 else 1)
 # ========================= Building the Executable ========================== #
-
-
-# Why am I not using Make?
-# 1.)   I want total control over the system. 
-#       Occasionally I want to have some logic
-#       in my compilation process (like searching for missing files).
-# 2.)   Realistically our projects are 'small' enough 
-#       this will not matter.
-# 3.)   Feel free to implement your own make files or autogenerate it from this
-#       script
-# 4.)   It is handy to know Python
 
 
